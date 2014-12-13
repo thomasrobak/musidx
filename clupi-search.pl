626464ac9bfe5836f78269dace47317fb941893f
Thomas Robak <thomas@robak.at>
2014-12-14 00:31:36 +0100

    scripts and so

---
+++ clupi-search.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+use Search::Elasticsearch;
+use Data::Dumper;
+use Text::Table;
+use File::Basename;
+
+sub cleanDur {
+	(my $p) = @_;
+	$p =~ s/ \(approx\)//;
+	$p;
+}
+
+sub cleanPath {
+	(my $p) = @_;
+	$p =~ s/\/Volumes\/v2\///;
+	$p;
+}
+
+my $e = Search::Elasticsearch->new(
+	nodes => [
+		"master1.clupi:9200"
+	]
+);
+
+(my $query,my $from,my $size) = @ARGV;
+
+if(!$from) {
+	$from = 0;
+}
+
+if(!$size) {
+	$size = 10;
+}
+
+my $result = $e->search(
+	index 	=> 'media',
+	fields	=> ['Artist','Title','AudioBitrate','SourceFile','AvgBitrate','Duration'],
+	body	=> {
+		from	=> $from,
+		size	=> $size,
+		query	=> {
+			bool	=> {
+				should	=> [
+					{ match => {Artist => $query}},
+					{ match => {Title => $query}},
+					{ match => {SourceFile => $query}},
+				]
+			}
+		}
+	}
+);
+
+#print Dumper($result->{'hits'}->{'hits'});
+
+my $rb = Text::Table->new(
+	"Artist","Title","Bitrate","Length","Duration"
+);
+
+die "no results" unless $result;
+my @hits = @{$result->{hits}->{hits}};
+#print Dumper(@hits);
+foreach $hit(@hits){
+	my $bitrate = $hit->{fields}->{AudioBitrate}[0] 
+			? $hit->{fields}->{AudioBitrate}[0]
+			: $hit->{fields}->{AvgBitrate}[0];
+	$rb->add(
+		$hit->{fields}->{Artist}[0],
+		$hit->{fields}->{Title}[0],
+		$bitrate,
+		(cleanDur $hit->{fields}->{Duration}[0]),
+		(cleanPath $hit->{fields}->{SourceFile}[0]),
+		#basename($hit->{fields}->{SourceFile}[0]),
+	);
+}
+print $rb;
