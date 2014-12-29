59c387fc7eb232ee1c61429d3316ecd89e81942c
Thomas Robak <thoams@robak.at>
2014-12-29 23:24:02 +0100

    remove wildcard

--- service/src/main/java/at/robak/musicservice/dao/files/ESMusicIndexDAO.java
+++ service/src/main/java/at/robak/musicservice/dao/files/ESMusicIndexDAO.java
@@ -51,7 +51,7 @@ public class ESMusicIndexDAO implements MusicIndexDAO{
     }
 
     private QueryBuilder buildQuery(String term) {
-        term = term.replaceAll(" ", "*");
+        //term = term.replaceAll(" ", "*");
         log.info(String.format("Searching for %s with fields %s", term, Arrays.toString(settings.getSearchFields())));
         return QueryBuilders.multiMatchQuery(term, settings.getSearchFields());
 //        return QueryBuilders.boolQuery()
